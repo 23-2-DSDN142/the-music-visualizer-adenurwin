d8e79adc0ff5f97e0fa2f1874f3d4f59e4fe0f16
adenurwin <adenurwin@gmail.com>
2023-10-20 16:19:13 +1300

    final

--- music_starter.js
+++ music_starter.js
@@ -1,18 +1,17 @@
-// Initialize variables
-let firstRun = true; // Flag to run certain setup code only on the first frame
-let earthImg; // Variable to store an image of Earth
-let moonImg; // Variable to store an image of the Moon
-let space_manImg; // Variable to store an image of a spaceman
-let manDepth = 10; // Initial depth of the spaceman
-let manInc = true; // Flag to control spaceman's depth increment direction
-let stars = []; // Array to store star positions
+let firstRun = true; // Flag for initial setup
+let earthImg; // Image of Earth
+let moonImg; // Image of the Moon
+let space_manImg; // Image of a spaceman
+let manDepth = 10; // Initial spaceman depth
+let manInc = true; // Flag for depth direction
+let stars = []; // Array for star positions
 let numStars = 100; // Number of stars
-let starSpeed = 5; // Speed at which stars move across the screen
-let coverAlpha = 0; // Initial alpha value for the white cover
+let starSpeed = 5; // Speed of stars
+let coverAlpha = 0; // Initial green cover alpha
 
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   if (firstRun) {
-    // Set up initial configuration only on the first frame
+    // Initial setup
     rectMode(CENTER);
     earthImg = loadImage('earth.png');
     moonImg = loadImage('moon.png');
@@ -27,7 +26,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     }
   }
 
-  // Move stars horizontally across the screen
+  // Move stars
   for (let star of stars) {
     star.x -= starSpeed * (drum / 100);
     if (star.x < 0) {
@@ -36,7 +35,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     }
   }
 
-  background(0); // Set the background color to black
+  background("#0D0208"); // Set background color
 
   fill(255);
   noStroke();
@@ -45,11 +44,11 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     ellipse(star.x, star.y, 2, 2);
   }
 
-  // Draw images of Earth and the Moon with dynamic positions based on bass
+  // Draw Earth and Moon images with dynamic positions based on bass
   image(earthImg, width / 2 - (500 + bass / 2) / 1 - manDepth / 10, 1 * height / 5 - (100 + bass / 2) / 2, 500 + bass / 2, 500 + bass / 2);
   image(moonImg, width / 2 + manDepth / 50 + 100, height / 2 - 50, 100, 100);
 
-  // Control the depth of the spaceman
+  // Control spaceman depth
   if (manDepth <= -100) {
     manInc = true;
   }
@@ -57,25 +56,25 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     manInc = false;
   }
 
-  // Adjust spaceman's depth based on the 'other' parameter
+  // Adjust spaceman depth based on 'other' parameter
   if (manInc) {
     manDepth = manDepth - (other - 90) / 10;
   } else {
     manDepth = manDepth + (other - 90) / 10;
   }
 
-  // Draw the spaceman image with dynamic position and size based on drum and manDepth
+  // Draw spaceman image with dynamic position and size based on drum and manDepth
   image(space_manImg, width / 2 - drum / 4 + manDepth / 5, height / 2 - drum / 2, drum / 2 + 50, drum / 2 + 50);
 
-  // Adjust the alpha of the white cover based on vocal
-  coverAlpha = map(100 - vocal, 0, 100, 20, 0); // Reduced maximum alpha
+  // Adjust green cover alpha based on vocal
+  coverAlpha = map(100 - vocal, 0, 100, 20, 0);
 
-  // Draw a white cover with variable transparency
+  // Draw green cover with variable transparency
   noStroke();
-  fill(255, coverAlpha);
+  fill(0, 255, 65, coverAlpha);
   rect(width / 2, height / 2, width, height);
 
-  // Draw a green waveform pattern based on the vocal input
+  // Draw green waveform pattern based on vocal input
   stroke("#00FF41");
   strokeWeight(4);
   noFill();
@@ -99,7 +98,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
   noFill();
   strokeWeight(200);
-  stroke("grey");
-  // Draw a grey ellipse in the background
+  stroke("#000B00");
+  // Draw green ellipse in the background
   ellipse(width / 2, height / 2, width + 200, height + 200);
 }
